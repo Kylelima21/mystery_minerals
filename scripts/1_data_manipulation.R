0bc8594d52a3f980703120e0148b6a5b9abfc9e8
Kyle Lima <klima@schoodicinstitue.org>
2022-05-06 11:19:29 -0400

    start of manipulation for classifying minerals

---
+++ scripts/1_data_manipulation.R
@@ -0,0 +1,30 @@
+#Determining the identity of some mystery minerals scanned with the PSR+
+#Kyle Lima and Peter Nelson, 2022
+
+
+
+#------------------------------------------------#
+####           Packages Required              ####
+#------------------------------------------------#
+
+require(tidyverse)
+require(spectrolab)
+
+
+
+#------------------------------------------------#
+####            Read in the data              ####
+#------------------------------------------------#
+
+mineral.spec <- read_spectra(path = "data/raw")
+
+
+
+#------------------------------------------------#
+####       Manipulate for classifying         ####
+#------------------------------------------------#
+
+
+
+
+
